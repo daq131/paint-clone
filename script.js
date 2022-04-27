53af5ed939509984ed0bca736811b645655a664b
PRO 13 <pro13@MacBook-Pro-PRO.local>
2022-04-27 14:10:09 +0200

    downl a pic

--- script.js
+++ script.js
@@ -205,12 +205,14 @@ clearStorageBtn.addEventListener('click', () => {
 });
 
 // // Download Image
-// downloadBtn.addEventListener('click', () => {
+downloadBtn.addEventListener('click', () => {
+  downloadBtn.href = canvas.toDataURL('image/jpeg', 1);
+  downloadBtn.download = 'paint-example.jpeg';
 
 //   // Active Tool
-//   activeToolEl.textContent = 'Image File Saved';
-//   setTimeout(switchToBrush, 1500);
-// });
+  activeToolEl.textContent = 'Image File Saved';
+  setTimeout(switchToBrush, 1500);
+});
 
 // // Event Listener
 brushIcon.addEventListener('click', switchToBrush);
